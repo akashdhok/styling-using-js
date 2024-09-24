72abcdd88d67ad370af0852ed3f899174faf69a8
Akash Dhok <akashdhok1265@gmail.com>
2024-09-24 12:43:24 +0530

    first commit

---
+++ day2/script.js
@@ -0,0 +1,20 @@
+// function dom(){
+//     let a = document.querySelector("#para");
+//     a.style.color = "red"
+//     a.style.fontSize = "30px"
+// }
+function dom(){
+    let a =  document.querySelector("#body");
+    a.style.backgroundColor = 'black'
+    a.style.color = "white"
+}
+function light(){
+    let b = document.querySelector("#body")
+    b.style.backgroundColor = "white"
+    b.style.color = "black"
+}
+function change(){
+let c = document.querySelector("#img1")
+ c.style.backgroundImage = "url('https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649')"
+}
+    
---
+++ script.js
@@ -0,0 +1,34 @@
+//    ==============================Moving one page to another==========================
+//    function check1(){
+//    let res =  window.confirm("Do you want to log-out")
+//    if (res) {
+//       window.location.href = "login.html "
+//    } else {
+//     alert("Invalid Syntax")
+    
+//    }
+// }
+// check1()
+// ================================even odd============================
+// function even(){
+//     let num = parseInt(window.prompt("Enter a number"))
+//     if (num%2===0) {
+//         window.alert("This is an even number")
+//     } else {
+//         window.alert("This is an odd number")
+        
+//     }
+// }
+// even()
+function check1(){
+    let res = window.confirm("Do you want to log out")
+    if (res) {
+        window.location.href = "login.html"
+    } else {
+        window.alert("Syntax error")
+    }
+}
+check1()
+akash = ()=>{
+    
+}
