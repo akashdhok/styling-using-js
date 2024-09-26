607ae13c4292288f980cbe5f77f6c2913c9ff9e6
Akash Dhok <akashdhok1265@gmail.com>
2024-09-26 11:38:11 +0530

    update

---
+++ practice/script.js
@@ -0,0 +1,16 @@
+function one(){
+    let a = document.querySelector("#img")
+    a.src = "./one.jpg"
+}
+function two(){
+    let a = document.querySelector("#img")
+    a.src = "./two.jpg"
+}
+function three(){
+    let a = document.querySelector("#img")
+    a.src = "./three.jpg"
+}
+function four(){
+    let a = document.querySelector("#img")
+    a.src = "./four.jpg"
+}
