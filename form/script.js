0371452a1e860f758f3f7b13f56efa01947ed274
Akash Dhok <akashdhok1265@gmail.com>
2024-10-01 12:06:54 +0530

    commit updated

---
+++ form/script.js
@@ -0,0 +1,8 @@
+function name(){
+    let name = document.querySelector("#name").value
+    let email = document.querySelector("#email").value
+    let pass = document.querySelector("#pass").value
+    let conpass = document.querySelector("#conpass").value
+     console.log(name , email , pass , conpass)
+   return false
+}
---
+++ form2/userinput.js
@@ -0,0 +1,35 @@
+
+
+function registration(){
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let mobile = document.querySelector('#mobile').value;
+    let password = document.querySelector('#password').value;
+    let cpassword = document.querySelector('#cpassword').value;
+
+    console.log(name,email,mobile,password,cpassword);
+
+    if(name === ""){
+        document.querySelector('#errorname').innerHTML = "please enter your name";
+        let selectname = document.querySelector('#name');
+        selectname.style.borderColor = "red"
+        selectname.style.outlineColor = "red"
+        return false;
+    }
+    else if(email === ""){
+        document.querySelector('#erroremail').innerHTML = "please enter valid email";
+        let selectname = document.querySelector('#email');
+        selectname.style.borderColor = "red"
+        selectname.style.outlineColor = "red"
+        return false;
+    } 
+}
+function s(){
+    let selectedinput = document.querySelector('#name');
+    let selecterrorname = document.querySelector('#errorname');
+    selecterrorname.innerHTML = ""
+    selectedinput.style.borderColor = "black";
+    selectedinput.style.outlineColor = "black";
+}
+
+// calling functions with arguments or parameter
